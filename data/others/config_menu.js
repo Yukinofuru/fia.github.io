b541f26a7d0ff2a3ae9323f73b976d5f12d40cbe
yukinofuru10242257 <yukinofuru.1024.2257@gmail.com>
2026-06-08 18:12:22 +0900

    no message

--- data/others/config_menu.js
+++
@@ -1,8 +0,0 @@
-var config_menu = {
- config: function(){
- tyrano.plugin.kag.ftag.startTag("sleepgame",{storage:"config.ks",next:false});
- setTimeout(function(){
- $('.layer.layer_menu').css({'display':'none'});
- },200);
- },
-};
